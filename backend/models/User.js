9b8aeb7584f54d81f71273d901ff06e48c3b3022
Nathan King <hello@nathanking.io>
2026-05-30 18:05:52 +1000

    Add registration API validation

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    password: { type: String, required: true, minlength: 6 },
     university: { type: String },
     address: { type: String },
 });
