6ee2f29f8739f8997170f48f1be6fff156072d7f
Nathan King <hello@nathanking.io>
2026-05-31 11:05:34 +1000

    Change axios to live baseURL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
-  headers: { 'Content-Type': 'application/json' },
+  // baseURL: "http://localhost:5001", // local
+  baseURL: "http://54.162.0.232:5001", // live
+  headers: { "Content-Type": "application/json" },
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const storedUser = localStorage.getItem('user');
+  const storedUser = localStorage.getItem("user");
 
   if (storedUser) {
     const user = JSON.parse(storedUser);
